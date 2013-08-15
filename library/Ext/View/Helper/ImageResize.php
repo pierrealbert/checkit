a19fce9358538e115e76a82dfe55588ecb94f343
fpinger <jonjonson7@gmail.com>
2013-08-15 22:59:16 +1100

    Update property details

--- library/Ext/View/Helper/ImageResize.php
+++ library/Ext/View/Helper/ImageResize.php
@@ -2,9 +2,17 @@
 
 class Ext_View_Helper_ImageResize extends Zend_View_Helper_Abstract
 {
-    public function imageResize($width, $height, $image)
+    public function imageResize($width, $height, $image, $attribs=array())
     {
-        $image = "<img src=\"/image/by/w/{$width}/h/{$height}/i/" . str_replace("/", '_', $image) . "\" alt=\"{$image}_{$width}x{$height}\" />";
+        $attr_string = '';
+
+        if (is_array($attribs) && $attribs) {
+            foreach ($attribs as $name => $value) {
+                $attr_string .= " {$name}=\"{$value}\"";
+            }
+        }
+
+        $image = "<img src=\"/image/by/w/{$width}/h/{$height}/i/" . str_replace("/", '_', $image) . "\" alt=\"{$image}_{$width}x{$height}\"{$attr_string} />";
 
         return $image;
     }
