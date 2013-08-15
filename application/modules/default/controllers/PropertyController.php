370be0b8caa76ff862ba4709dcb09f2a5063c6c9
fpinger <jonjonson7@gmail.com>
2013-08-15 14:23:13 +1100

    Update property details

--- application/modules/default/controllers/PropertyController.php
+++ application/modules/default/controllers/PropertyController.php
@@ -4,5 +4,25 @@ class PropertyController extends Zend_Controller_Action
 {
     public function detailAction()
     {
+                    
+        $query = Doctrine::getTable('Model_Property')->createQuery('p')
+            ->select('p.*, (p.amount_of_rent_excluding_charges + p.amount_of_charges) as price')
+            ->where('p.id = ?', $this->_getParam('item', 0))
+            ->andWhere('p.is_published = ?', 1);
+
+        $property = $query->fetchOne();
+
+        if (!$property) {
+
+            $this->_helper->redirector('index', 'index');
+        }
+
+        $this->view->property      = $property;
+        $this->view->property_type = Model_Property::getTypes();
+
+        $options = Zend_Controller_Action_HelperBroker::getStaticHelper('settings')
+            ->get('services.googleMaps');
+
+        $this->view->google_api_key = $options['apiKey'];
     }
 }
