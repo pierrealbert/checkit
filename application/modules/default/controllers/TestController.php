58924d7ade17736bd63911905b858f4ce512b0a3
fpinger <jonjonson7@gmail.com>
2013-08-16 11:35:04 +1100

    Bug fix test list property

--- application/modules/default/controllers/TestController.php
+++ application/modules/default/controllers/TestController.php
@@ -6,7 +6,7 @@ class TestController extends Zend_Controller_Action
     {
         $ads = Doctrine::getTable('Model_Property')->createQuery('p')
             ->select('p.*, (p.amount_of_rent_excluding_charges + p.amount_of_charges) as price')
-            ->where('p.is_published = ?', 1);
+            ->where('p.is_published = ?', 1)
             ->execute();
 
         $this->view->ads = $ads;
