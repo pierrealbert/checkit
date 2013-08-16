923b60b70450100443281ac7c441638a2dfeb332
fpinger <jonjonson7@gmail.com>
2013-08-16 13:14:41 +1100

    Added tow slideshow property details

--- library/Ext/View/Helper/ImageResize.php
+++ library/Ext/View/Helper/ImageResize.php
@@ -12,7 +12,7 @@ class Ext_View_Helper_ImageResize extends Zend_View_Helper_Abstract
             }
         }
 
-        $image = "<img src=\"/image/by/w/{$width}/h/{$height}/i/" . str_replace("/", '_', $image) . "\" alt=\"{$image}_{$width}x{$height}\"{$attr_string} />";
+        $image = "<img src=\"/image/by/w/{$width}/h/{$height}/i/" . str_replace("/", '_', $image) . "\" alt=\"{$width}x{$height}_{$image}\"{$attr_string} />";
 
         return $image;
     }
