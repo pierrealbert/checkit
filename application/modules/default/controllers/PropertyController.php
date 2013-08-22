28c882cf63a4a90f8b529e92513f0dd0ad0f698e
Alex <molchanov1983@bigmir.net>
2013-08-22 11:25:04 +0300

    create ajax request if we click add to bookmark

--- application/modules/default/controllers/PropertyController.php
+++ application/modules/default/controllers/PropertyController.php
@@ -4,7 +4,6 @@ class PropertyController extends Zend_Controller_Action
 {
     public function detailAction()
     {
-                    
         $query = Doctrine::getTable('Model_Property')->createQuery('p')
             ->select('p.*, (p.amount_of_rent_excluding_charges + p.amount_of_charges) as price')
             ->where('p.id = ?', $this->_getParam('item', 0))
@@ -63,4 +62,79 @@ class PropertyController extends Zend_Controller_Action
 
         return $result;
     }
+    
+
+    /**
+     * AJAX
+     * this method for adding information to db table favorite
+     * when we ckick button add to bookmark we call this method using ajax
+     * @return 
+     */
+    public function addtobookmarkajaxAction()
+    {
+	$returnData = new stdClass();
+	//if it is ajax
+	if ($this->getRequest()->isXmlHttpRequest())
+	{	   
+	    //if it is post request 
+	    if ($this->getRequest()->isPost()) 
+	    {
+		//here i need to know user id and property id
+		$auth = Zend_Auth::getInstance();
+		$userId = $auth->getIdentity();
+		//if we donot have userid it means we donot logined
+		if(empty($userId))
+		{
+		    $returnData->redirectUrl = $this->_helper->url('' , 'login');
+		    $jsonStr = $this->_helper->json($returnData);
+		    echo $jsonStr;
+		    die;
+		}//endif
+		//get property id
+
+		$propertyId = '';
+		echo "<pre>";
+		print_r($propertyId);
+		echo "</pre>";
+		die;
+	    }
+	    else
+	    {
+		// it is not post request
+		$this->_helper->redirector('index', 'index');
+		die;
+	    }//endif
+	}
+	else 
+	{
+	    // ... Do normal controller logic here (To catch non ajax calls to the script)
+	    $this->_helper->redirector('index', 'index');
+	}//endif
+
+    }//endfunc
+     /**
+     * AJAX
+     * this method for adding information to db table favorite
+     * 
+     */
+    public function addtoshareajaxAction()
+    {
+	echo "<pre>";
+	print_r(2);
+	echo "</pre>";
+	die;
+    }
+       /**
+     * AJAX
+     * this method for adding information to db table favorite
+     * 
+     */
+    public function addtoapplyajaxAction()
+    {
+	echo "<pre>";
+	print_r(3);
+	echo "</pre>";
+	die;
+    }
+  
 }
