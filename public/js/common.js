9a89530bbca916174fc7bbee4a3e13f8b2430279
pkamenev <pkamenev@gmail.com>
2013-09-27 16:13:08 +0700

    Modal Dialog - Registration

--- public/js/common.js
+++ public/js/common.js
@@ -2,31 +2,17 @@
 $(function() {
 
     $('#modal-dialog').dialog({
-        autoOpen: false,    
-        modal: true,
-        buttons: {
-        },
-        close: function() {
-        }
+        autoOpen: false,
+        modal: true
     });
 
-    $('.open-in-modal').click(function(event) {        
+    $('.open-in-modal').click(function(event) {
         var anchor = $(event.target);
         event.preventDefault();
-        
+
         $.get(anchor.attr('href'), function(data) {
-            $('#modal-dialog').html(data);                 
+            $('#modal-dialog').html(data);
             $('#modal-dialog').dialog('open');
         });
     });
-    
-    $('#login-button').click(function(event) { 
-        
-        alert('ddd');
-         event.preventDefault();
-         $.post('/login', function(data) {
-             $('#modal-dialog').html(data);
-             $('#modal-dialog').dialog('open');
-         });        
-     });    
 });
