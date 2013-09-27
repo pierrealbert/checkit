d7312bca32c543ac0d3fe68104f89bfe587fdfcd
sergeyloysha <sergeyloysha@gmail.com>
2013-09-27 21:18:11 +0300

    New modals plugin

--- public/js/common.js
+++ public/js/common.js
@@ -1,20 +1,24 @@
 
 $(function() {
 
-    $('#modal-dialog').dialog({
-        autoOpen: false,
-        modal: true,
-        width: 630,
-        title: 'Demo title'
+    $('.open-in-modal').click(function(e) {
+        e.preventDefault();
+
+        var anchor = $(this).attr('href');
+
+        $('#modal').modal().open({
+            onOpen: function(el, options){
+                $.get(anchor, function(response){
+                    $('#modal .modal-content-container').html(response);
+                });
+            }
+        });
+
     });
 
-    $('.open-in-modal').click(function(event) {
-        var anchor = $(event.target);
-        event.preventDefault();
-
-        $.get(anchor.attr('href'), function(data) {
-            $('#modal-dialog').html(data);
-            $('#modal-dialog').dialog('open');
-        });
+    $('#modal .modal-close').on('click', function(e){
+        e.preventDefault();
+        $.modal().close();
+        $('#modal .modal-content-container').empty();
     });
 });
